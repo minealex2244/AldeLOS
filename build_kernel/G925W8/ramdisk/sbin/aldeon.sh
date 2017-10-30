b34783c75420bf27eca556b606ad0d841e43dc42
minealex2244 <minealex2244@gmail.com>
2017-10-30 18:48:10 +0200

    Synced sources with Aldeon kernel

--- build_kernel/G925W8/ramdisk/sbin/aldeon.sh
+++ build_kernel/G925W8/ramdisk/sbin/aldeon.sh
@@ -14,14 +14,35 @@ sh /sbin/resetprop.sh > /dev/null;
 # Mount rootfs and system as RW
 
 mount -o rw,remount rootfs;
-mount -o rw,remount /system;
 
 # Fix permissions
 
-chmod 0666 /sys/devices/system/cpu/cpu4/cpufreq/interactive/param_index
-chmod 0666 /sys/devices/system/cpu/cpu4/cpufreq/interactive/cpu_util
-chmod 0666 /sys/devices/system/cpu/cpu0/cpufreq/interactive/cpu_util
 chmod 0666 /sys/devices/14ac0000.mali/dvfs_governor
+chmod 0666 /sys/module/workqueue/parameters/power_efficient
+
+# Set stock clock values
+
+echo 400000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo 400000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
+echo 400000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
+echo 400000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
+echo 400000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
+echo 400000 > /sys/devices/system/cpu/cpu5/cpufreq/scaling_min_freq
+echo 400000 > /sys/devices/system/cpu/cpu6/cpufreq/scaling_min_freq
+echo 400000 > /sys/devices/system/cpu/cpu7/cpufreq/scaling_min_freq
+
+echo 1500000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo 1500000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+echo 1500000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+echo 1500000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+echo 2100000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
+echo 2100000 > /sys/devices/system/cpu/cpu5/cpufreq/scaling_max_freq
+echo 2100000 > /sys/devices/system/cpu/cpu6/cpufreq/scaling_max_freq
+echo 2100000 > /sys/devices/system/cpu/cpu7/cpufreq/scaling_max_freq
+
+echo 1 > /sys/devices/14ac0000.mali/dvfs_governor
+echo 266 > /sys/devices/platform/gpusysfs/gpu_min_clock
+echo 700 > /sys/devices/platform/gpusysfs/gpu_max_clock
 
 # Synapse
 
